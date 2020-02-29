c287b43918277974fd554ffc7ebba31777f540ee
Isaac Krementsov <isaackrementsov@gmail.com>
2020-02-29 17:29:22 -0500

    Fixed login issue

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(session({
     cookie: {httpOnly: true}
 }));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3030;
 app.listen(port, (err) => {
     if(err) console.log('Server error:', err);
     else console.log('Success running server on port', port);
